889590c440668fa90dd829bbe4553391094a883d
N-Ziermann <n-ziermann@protonmail.com>
2022-06-03 16:38:44 +0200

    Turn into chrome extension

--- src/App.js
+++ src/App.js
@@ -2,22 +2,24 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  function test(){
+    /* eslint-disable no-undef */
+    chrome.tabs.query({active: true, currentWindow:true}, tabs=>{
+      const activeTabId = tabs[0].id;
+      chrome.scripting.executeScript(
+        {
+          target: {tabId: activeTabId},
+          //function: ()=>alert("React chrome extension alert")
+          function: ()=>{document.body.innerHTML = "Hello World"}
+        }
+      )
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={test}>Send alert</button>
     </div>
   );
 }
